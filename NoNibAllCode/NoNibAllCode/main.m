75ba31e5baa66b8b285f2cc98de0bbafa4124625
Michael Mellinger <mmellinger66@gmail.com>
2013-11-01 14:59:34 -0400

    Initial Commit

---
+++ NoNibAllCode/NoNibAllCode/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NoNibAllCode
+//
+//  Created by Michael Mellinger on 11/1/13.
+//  Copyright (c) 2013 h4labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
